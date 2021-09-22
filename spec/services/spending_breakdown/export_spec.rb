2beff48e74ad90a728e6a82080d313a28f264e59
meyric <meyric@dxw.com>
2021-09-22 16:09:02 +0100

    WIP

--- spec/services/spending_breakdown/export_spec.rb
+++ spec/services/spending_breakdown/export_spec.rb
@@ -11,19 +11,58 @@ RSpec.describe SpendingBreakdown::Export do
   let!(:positive_refund_adjustment) { create(:adjustment, :refund, parent_activity: activity, value: 50, financial_quarter: 1, financial_year: 2020) }
   let!(:negative_refund_adjustment) { create(:adjustment, :refund, parent_activity: activity, value: -200, financial_quarter: 1, financial_year: 2020) }
 
+  let!(:actual_fq4) { create(:actual, parent_activity: activity, value: 100, financial_quarter: 4, financial_year: 2020) }
+  let!(:refund_fq4) { create(:refund, parent_activity: activity, value: -200, financial_quarter: 4, financial_year: 2020) }
+
+  let!(:positive_actual_adjustment_fq4) { create(:adjustment, :actual, parent_activity: activity, value: 200, financial_quarter: 4, financial_year: 2020) }
+  let!(:negative_actual_adjustment_fq4) { create(:adjustment, :actual, parent_activity: activity, value: -100, financial_quarter: 4, financial_year: 2020) }
+
+  let!(:positive_refund_adjustment_fq4) { create(:adjustment, :refund, parent_activity: activity, value: 50, financial_quarter: 4, financial_year: 2020) }
+  let!(:negative_refund_adjustment_fq4) { create(:adjustment, :refund, parent_activity: activity, value: -200, financial_quarter: 4, financial_year: 2020) }
+
   subject { SpendingBreakdown::Export.new(organisation: organisation, source_fund: source_fund) }
 
+  def value_for_header(header_name)
+    subject.rows.first[subject.headers.index(header_name)]
+  end
+
   describe "#headers" do
-    it "returns a list of column headings" do
-      expect(subject.headers).to eql([
+    it "includes the five headings that describe the activity" do
+      expect(subject.headers[0..4]).to eql([
         "RODA identifier",
         "Delivery partner identifier",
         "Delivery partner organisation",
         "Title",
         "Level",
+      ])
+    end
+
+    it "includes the three heading that descibe the finances for financial quarter 1 2020-2021" do
+      expect(subject.headers[5..7]).to eql([
         "Actual spend FQ1 2020-2021",
         "Refund FQ1 2020-2021",
         "Actual net FQ1 2020-2021",
+      ])
+    end
+
+    it "includes the three heading that descibe the finances for financial quarter 4 2020-2021" do
+      expect(subject.headers[14..16]).to eql([
+        "Actual spend FQ4 2020-2021",
+        "Refund FQ4 2020-2021",
+        "Actual net FQ4 2020-2021",
+      ])
+    end
+
+    it "includes the three heading that descibe the finances for financial quarters inbetween" do
+      expect(subject.headers[8..10]).to eql([
+        "Actual spend FQ2 2020-2021",
+        "Refund FQ2 2020-2021",
+        "Actual net FQ2 2020-2021",
+      ])
+      expect(subject.headers[11..13]).to eql([
+        "Actual spend FQ3 2020-2021",
+        "Refund FQ3 2020-2021",
+        "Actual net FQ3 2020-2021",
       ])
     end
   end
@@ -32,26 +71,34 @@ RSpec.describe SpendingBreakdown::Export do
     describe "non financial data" do
       it "conatins the appropriate values" do
         aggregate_failures do
-          expect(subject.rows.first[0]).to eql(activity.roda_identifier)
-          expect(subject.rows.first[1]).to eql(activity.delivery_partner_identifier)
-          expect(subject.rows.first[2]).to eql(activity.organisation.name)
-          expect(subject.rows.first[3]).to eql(activity.title)
-          expect(subject.rows.first[4]).to eql("Project (level C)")
+          expect(value_for_header("RODA identifier")).to eql(activity.roda_identifier)
+          expect(value_for_header("Delivery partner identifier")).to eql(activity.delivery_partner_identifier)
+          expect(value_for_header("Delivery partner organisation")).to eql(activity.organisation.name)
+          expect(value_for_header("Title")).to eql(activity.title)
+          expect(value_for_header("Level")).to eql("Project (level C)")
         end
       end
     end
 
     describe "financial data" do
-      it "contains the total actuals including adjustments at position X" do
-        expect(subject.rows.first[5].to_s).to eql("200.0")
+      it "contains the financial data for financial quarter 1 2020-2021" do
+        aggregate_failures do
+          expect(subject.rows.first[5].to_s).to eql("200.0")
+          expect(subject.rows.first[6].to_s).to eql("-350.0")
+          expect(subject.rows.first[7].to_s).to eql("-150.0")
+        end
       end
 
-      it "contains the total refunds including adjustments at position Y" do
-        expect(subject.rows.first[6].to_s).to eql("-350.0")
+      it "contains the financial data for financial quarter 4 2020-2021" do
+        aggregate_failures do
+          expect(subject.rows.first[11].to_s).to eql("200.0")
+          expect(subject.rows.first[12].to_s).to eql("-350.0")
+          expect(subject.rows.first[13].to_s).to eql("-150.0")
+        end
       end
 
-      it "contains total actuals less total refunds, including all adjustments" do
-        expect(subject.rows.first[7].to_s).to eql("-150.0")
+      it "containts zeros value for the financial quarters inbetween" do
+
       end
     end
   end
